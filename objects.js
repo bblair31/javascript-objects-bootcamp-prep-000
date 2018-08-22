c723d5bb34e805a5a7feccfba9a36c45e45a7617
jbb341@hotmail.com <jbb341@hotmail.com>
2018-08-22 18:42:06 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,2 +1,6 @@
-var playlist = { artistname: "songtitle"
+var playlist = { artistName: "songTitle"
+}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  return Object.assign({}, playlist, { [artistName]: songTitle})
 }
