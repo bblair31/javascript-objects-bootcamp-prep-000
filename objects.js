0fb2ec31317713d438035ead5023fe3728887c6c
jbb341@hotmail.com <jbb341@hotmail.com>
2018-08-22 19:52:29 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -8,6 +8,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return playlist
 }
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist.'Slowdive'
+  delete playlist.Slowdive
   return playlist
 }
