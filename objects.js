33cd94dda1a501ce9639f72e1c8eea3e16bfbe10
jbb341@hotmail.com <jbb341@hotmail.com>
2018-08-22 19:49:35 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
-var playlist = new Object( { 
+var playlist = { 
   'Slowdive': 'Alison',
   'My Bloody Valentine': 'Sometimes'
-})
+}
 
 function updatePlaylist(playlist, artistname, songTitle) {
   return Object.assign({}, playlist, { ['Phil Ochs']: "Here's to the State of Mississippi"})
