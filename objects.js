64760633dc98fbbd416058768c69c643fa042152
jbb341@hotmail.com <jbb341@hotmail.com>
2018-08-22 19:12:09 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,5 +4,4 @@ var playlist = {
 
 function updatePlaylist(playlist, artistName, songTitle) {
   Object.assign({}, playlist, { ["Phil Ochs"]: "Here's to the State of Mississippi"})
-  return playlist
 }
